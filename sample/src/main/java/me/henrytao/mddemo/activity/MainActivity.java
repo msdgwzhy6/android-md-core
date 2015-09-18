3827d5bee74ea21ce24f4971caa1b30a4dc9f032
Henry Tao <hi@henrytao.me>
2015-09-18 22:11:56 +0700

    [v2] Init v2

--- sample/src/main/java/me/henrytao/mddemo/activity/MainActivity.java
+++ sample/src/main/java/me/henrytao/mddemo/activity/MainActivity.java
@@ -1,25 +1,29 @@
+/*
+ * Copyright 2015 "Henry Tao <hi@henrytao.me>"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.henrytao.mddemo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 
-import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import me.henrytao.mddemo.R;
-import me.henrytao.mddemo.fragment.ExampleCardViewFragment;
-import me.henrytao.mddemo.fragment.ExampleMdButtonFragment;
-import me.henrytao.mddemo.fragment.ExampleMdEditTextFragment;
-import me.henrytao.mddemo.fragment.ExampleMdListFragment;
-import me.henrytao.mddemo.fragment.InfoFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-  @Bind(R.id.toolbar)
-  Toolbar vToolbar;
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,51 +32,9 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case R.id.action_info:
-        startActivity(SimpleActivity.newIntent(this, InfoFragment.class, getString(R.string.text_about)));
-        return true;
-    }
-    return super.onOptionsItemSelected(item);
-  }
-
-  @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
-
-    setSupportActionBar(vToolbar);
-  }
-
-  @OnClick(R.id.goto_button)
-  protected void onGotoButtonClick() {
-    startActivity(SimpleActivity.newIntent(this, ExampleMdButtonFragment.class, "Button"));
-  }
-
-  @OnClick(R.id.goto_edit_text)
-  protected void onGotoEditTextClick() {
-    startActivity(SimpleActivity.newIntent(this, ExampleMdEditTextFragment.class, "EditText"));
-  }
-
-  @OnClick(R.id.goto_list_view)
-  protected void onGotoListViewClick() {
-    startActivity(SimpleActivity.newIntent(this, ExampleMdListFragment.class, "ListView"));
-  }
-
-  @OnClick(R.id.goto_textview)
-  protected void onGotoTextViewClick() {
-    startActivity(TypoActivity.newIntent(this));
-  }
-
-  @OnClick(R.id.goto_theme)
-  protected void onGotoThemeClick() {
-    startActivity(ThemeActivity.newIntent(this));
-  }
-
-  @OnClick(R.id.goto_card_view)
-  protected void onGototCardViewClick() {
-    startActivity(SimpleActivity.newIntent(this, ExampleCardViewFragment.class, "CardView"));
   }
 }
